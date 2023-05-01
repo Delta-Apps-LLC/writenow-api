ae2b8dcd15b27652eb6790c21a0e7dc78c23bc29
athibau2 <a.thibs98@gmail.com>
2023-05-01 00:25:13 -0600

    added localhost as cors origin

--- api/index.js
+++ api/index.js
@@ -3,7 +3,10 @@ const cors = require('cors')
 
 const port = process.env.PORT || 3001
 
-app.use(cors())
+app.use(cors({
+  // Change to hosted website url
+  origin: 'http://localhost:3000'
+}))
 
 app.listen(port, () => {
   console.log('server is running on port ' + port);

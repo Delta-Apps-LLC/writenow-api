905db940ca00b4a8f7abca108d230406ac083a30
athibau2 <a.thibs98@gmail.com>
2023-05-08 00:20:14 -0600

    changed health controller

--- api/controller/health.js
+++ api/controller/health.js
@@ -3,9 +3,12 @@ const health = require('../database/health')
 module.exports = function (pool) {
     return {
         async checkHealth(req, res) {
-            const peeps = await health.checkHealth(pool)
-	    console.log(peeps)
-            if (peeps !== null && peeps !== undefined) {
+            // const peeps = await health.checkHealth(pool)
+            const peeps = {
+                healthRes: 'HEllo there!'
+            }
+	        console.log(peeps)
+            if (peeps != null && peeps != undefined) {
                 res.enforcer.status(200).send(peeps)
             }
             else {

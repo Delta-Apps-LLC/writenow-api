3d187c0b98366359293f1acb7981e84191dee4e0
athibau2 <a.thibs98@gmail.com>
2023-09-19 16:22:03 -0600

    added cors headers

--- api/server.js
+++ api/server.js
@@ -121,13 +121,13 @@ passport.deserializeUser((id, done) => {
 const app = express()
 
 // Add CORS headers
-//app.use((req, res, next) => {
-//  res.setHeader('Access-Control-Allow-Origin', 'https://joinwritenow.com')
-//  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-//  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-//  res.setHeader('Access-Control-Allow-Credentials', true)
-//  next()
-//})
+app.use((req, res, next) => {
+ res.setHeader('Access-Control-Allow-Origin', 'https://joinwritenow.com')
+ res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+ res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+ res.setHeader('Access-Control-Allow-Credentials', true)
+ next()
+})
 
 app.use(cors({
 	origin: 'https://joinwritenow.com',
